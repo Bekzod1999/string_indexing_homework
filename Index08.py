0379081b7caf87434b6776a47344bdfe776e54db
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-19 19:27:52 +0500

    08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,47 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    k=0
+    if len(s) > 4:
+        if s[0] == '*':
+            k+=1
+        if s[1] == '*':
+            k+=1
+        if s[2] == '*':
+            k+=1
+        if s[3] == '*':
+            k+=1
+        if s[4] == '*':
+            k+=1
+    elif len(s) > 3:
+        if s[0] == '*':
+            k+=1
+        if s[1] == '*':
+            k+=1
+        if s[2] == '*':
+            k+=1
+        if s[3] == '*':
+            k+=1
+    elif len(s) > 2:
+        if s[0] == '*':
+            k+=1
+        if s[1] == '*':
+            k+=1
+        if s[2] == '*':
+            k+=1
+    elif len(s) > 1:
+        if s[0] == '*':
+            k+=1
+        if s[1] == '*':
+            k+=1  
+    elif len(s) > 0:
+        if s[0] == '*':
+            k+=1
+   
+    if k == 0:
+        k = bool(0)
+            
+    return k
+x=main('dj*od')
+print(x)
         
