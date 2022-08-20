a54e942005e477e208b31d3e80fcea0d28ae168e
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-20 10:13:42 +0500

    08

--- Index08.py
+++ Index08.py
@@ -8,21 +8,46 @@ def main(s):
     """
     k=0
     
+    if len(s) == 5:
+        if ord(s[0]) == 42:
+            k+=1
+        if ord(s[1]) == 42:
+            k+=1
+        if ord(s[2]) == 42:
+            k+=1
+        if ord(s[3]) == 42:
+            k+=1
+        if ord(s[4]) == 42:
+            k+=1
+    if len(s) == 4:
+        if ord(s[0]) == 42:
+            k+=1
+        if ord(s[1]) == 42:
+            k+=1
+        if ord(s[2]) == 42:
+            k+=1
+        if ord(s[3]) == 42:
+            k+=1
+    if len(s) == 3:
+        if ord(s[0]) == 42:
+            k+=1
+        if ord(s[1]) == 42:
+            k+=1
+        if ord(s[2]) == 42:
+                k+=1
+    if len(s) == 2:
+        if ord(s[0]) == 42:
+            k+=1
+        if ord(s[1]) == 42:
+            k+=1
+    if len(s) == 1:
+        if ord(s[0]) == 42:
+            k+=1
 
-    if ord(s[0]) == 42:
-        k+=1
-    if ord(s[1]) == 42:
-        k+=1
-    if ord(s[2]) == 42:
-        k+=1
-    if ord(s[3]) == 42:
-        k+=1
-    if ord(s[4]) == 42:
-        k+=1
     if k == 0:
         k = bool(0)
             
     return k
-x=main('*d*fs')
+x=main('g*od')
 print(x)
         
