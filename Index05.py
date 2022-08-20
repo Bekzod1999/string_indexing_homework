1bba32a25c58b194f6d8c292f3687031224f8d5c
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-20 09:47:17 +0500

    05

--- Index05.py
+++ Index05.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +7,19 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    # 1kld2
+    k = 0
+    if ord(s[0]) > 47 and ord(s[0]) < 58:
+        k+=1
+    if ord(s[1]) > 47 and ord(s[1]) < 58:
+        k+=1
+    if ord(s[2]) > 47 and ord(s[2]) < 58:
+        k+=1
+    if ord(s[3]) > 47 and ord(s[3]) < 58:
+        k+=1
+    if ord(s[4]) > 47 and ord(s[4]) < 58:
+        k+=1
+    
+    return k
+x=main('1s5d2')
+print(x)
