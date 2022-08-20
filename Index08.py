7c998743b1dc9abfcea5f8ec6f7894161c981218
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-20 15:33:34 +0500

    008

--- Index08.py
+++ Index08.py
@@ -8,20 +8,20 @@ def main(s):
     """
     k=0
     if s[0] == '*':
-        k+=1
+        k = 1
     if s[1] == '*':
-        k+=1
+        k = 2
     if s[2] == '*':
-        k+=1
+        k = 3
     if s[3] == '*':
-        k+=1
+        k = 4
     if s[4] == '*':
-        k+=1
+        k = 5
     if k == 0:
         k = bool(0)
             
     return k
-x=main('ggggg')
+x=main('good')
 print(x)
         
 
